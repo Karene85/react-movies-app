3373b0d5772b425a65d2176c8f3c64204433208c
karene85 <karenemody@gmail.com>
2021-02-13 10:22:17 +0530

    Added unit price, total price and book show button on book show page

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -17,12 +17,19 @@ import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
 import { Typography } from '@material-ui/core';
 
+
 class BookShow extends Component {
 
     constructor() {
         super();
         this.state = {
-            location: ""
+            location: "",
+            language: "",
+            showDate: "",
+            showTime: "",
+            tickets: 0,
+            unitPrice: 500,
+            availableTickets: 20
         }
     }
 
@@ -32,6 +39,22 @@ class BookShow extends Component {
 
     locationChangeHandler = (event) => {
         this.setState({location: event.target.value});
+    }
+
+    languageChangeHandler = (event) => {
+        this.setState({language: event.target.value});
+    }
+
+    showDateChangeHandler = (event) => {
+        this.setState({showDate: event.target.value});
+    }
+
+    showTimeChangeHandler = (event) => {
+        this.setState({showTime: event.target.value});
+    }
+
+    ticketsChangeHandler = (event) => {
+        this.setState({tickets: event.target.value});
     }
 
     render() {
@@ -56,6 +79,50 @@ class BookShow extends Component {
                                 ))}
                                 </Select>
                             </FormControl>
+                            <FormControl required className='formControl'>
+                                <InputLabel htmlFor='language'>Choose Language: </InputLabel>
+                                <Select
+                                value={this.state.language}
+                                onChange={this.languageChangeHandler}>
+                                {language.map(lang => (
+                                    <MenuItem key={'lang'+lang.id} value={lang.language}>{lang.language}</MenuItem>
+                                ))}
+                                </Select>
+                            </FormControl>
+                            <FormControl required className='formControl'>
+                                <InputLabel htmlFor='showDate'>Choose Show Date: </InputLabel>
+                                <Select
+                                value={this.state.showDate}
+                                onChange={this.showDateChangeHandler}>
+                                {showDate.map(date => (
+                                    <MenuItem key={'date'+date.id} value={date.showDate}>{date.showDate}</MenuItem>
+                                ))}
+                                </Select>
+                            </FormControl>
+                            <FormControl required className='formControl'>
+                                <InputLabel htmlFor='showTime'>Choose Show Time: </InputLabel>
+                                <Select
+                                value={this.state.showTime}
+                                onChange={this.showTimeChangeHandler}>
+                                {showTime.map(time => (
+                                    <MenuItem key={'time'+time.id} value={time.showTime}>{time.showTime}</MenuItem>
+                                ))}
+                                </Select>
+                            </FormControl>
+                            <FormControl required className='formControl'>
+                                <InputLabel htmlFor='tickets'>Tickets: ({this.state.availableTickets} available)</InputLabel>
+                                <Input id='tickets' value={this.state.tickets !== 0 ? this.state.tickets : ""} 
+                                onChange={this.ticketsChangeHandler}></Input>
+                            </FormControl><br/><br/>
+                            <Typography>
+                                Unit price: Rs. {this.state.unitPrice}
+                            </Typography><br/>
+                            <Typography>
+                                Total Price: Rs. {this.state.unitPrice * this.state.tickets}
+                            </Typography><br/><br/>
+                            <Button variant='contained' onClick={this.bookShowButtonHandler} color='primary'>
+                                BOOK SHOW
+                            </Button>
                         </CardContent>
                     </Card>
                 </div>
